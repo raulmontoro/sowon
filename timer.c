702b1f0f7303849bdedbbd544cd5e0b561174639
raulmontoro <raul@user.noreplay.git>
2024-06-15 15:45:54 +0200

    setting timers in timer.c. broken code

--- timer.c
+++ timer.c
@@ -2,7 +2,8 @@
 #include <time.h>
 
 
-/*  date and time c library 
+/*  REFERENCES 
+    date and time c library 
     https://en.cppreference.com/w/c/chrono
     https://en.cppreference.com/w/c/chrono/timespec_get
 
@@ -17,19 +18,41 @@
 
     timerfd_create()
     https://man7.org/linux/man-pages/man2/timerfd_create.2.html
-*/
 
-
-/*  REFERENCES 
     $ man 7 sigevent 
     $ man 7 signal
+
+    example
+    https://stackoverflow.com/questions/49860625/send-signal-at-specific-time-in-linux-c
 */
+
+
+/*  waiting for a signal to be caught
+        paused()
+            suspends execution until any signal is caught
+
+        sigsuspend()
+*/
+
+/*  accepting a signal synchronously
+        sigtimedwait()
+*/
+
 
 
 /*  signal handler
+    catching the signal
+        changing the default disposition of a signal:
+            Term, 
+            Ign, 
+            Core, 
+            Stop, 
+            Cont
 */
 sigaction
 
+
+/******************* CREATING TIMERS *******************/
 
 /*  signal to send on timer expiration
 */
@@ -41,47 +64,6 @@ struct sigevent sevp = {
     NULL,
 };
 
-
-
-/*  setting timers
-    https://www3.physnet.uni-hamburg.de/physnet/Tru64-Unix/HTML/APS33DTE/DOCU_007.HTM#sig-and-timers-sec
-*/
-
-/*  tv_sec*1e9 ns + tv_nsec     nanoseconds
-*/
-typedef struct timespec {
-    time_t tv_sec;  // Seconds
-    long tv_nsec;   // Nanoseconds
-} Timespec_t;
-
-Timespec_t timespec = { 1,      
-                        23e-9 
-                      };
-
-
-
-/*  both fields define one, only, period of time
-    it_value
-        0       timer disarmed
-        non-0   timer armed
-                timer first expiration time
-
-    it_interval
-        0       no reload timer
-                one-shot timer
-        non-0   periodic timer
-    flag
-        TIMER_ABSTIME
-            makes timer an absolute timer
-            difference with Epoch
-        no-flag
-            makes timer a relative timer
-            it_interval used
-*/
-struct itimerspec {
-    struct timespec it_interval;    // timer interval
-    struct timespec it_value;       // initial expiration
-};
 
 
 
@@ -121,15 +103,59 @@ void createTimer(clockid_t clockid,
 
 
 
-/*
 
 
+/******************* SETTING TIMERS *******************/
+
+/*  setting timers
+    https://www3.physnet.uni-hamburg.de/physnet/Tru64-Unix/HTML/APS33DTE/DOCU_007.HTM#sig-and-timers-sec
 */
-void setTimer(
-    timer_t timerid, 
-    int flags, 
-    const struct itimerspec *new_value,
-    struct itimerspec *old_value) {
+
+/*  tv_sec*1e9 ns + tv_nsec     nanoseconds
+*/
+typedef struct timespec {
+    time_t tv_sec;  // Seconds
+    long tv_nsec;   // Nanoseconds
+} Timespec_t;
+
+Timespec_t timespec = { 1,      
+                        23e-9 
+                      };
+
+
+/*  both fields define one, only, period of time
+    it_value
+        0       timer disarmed
+        non-0   timer armed
+                timer first expiration time
+
+    it_interval
+        0       no reload timer
+                one-shot timer
+        non-0   periodic timer
+    flag
+        TIMER_ABSTIME
+            makes timer an absolute timer
+            difference with Epoch
+        no-flag
+            makes timer a relative timer
+            it_interval used
+*/
+struct itimerspec {
+    struct timespec it_interval;    // timer interval
+    struct timespec it_value;       // initial expiration
+};
+
+
+
+
+/*  pre:
+    post:
+*/
+void setTimer(timer_t timerid, 
+              int flags, 
+              const struct itimerspec *new_value,
+              struct itimerspec *old_value) {
 
     timer_settime(
         timerid, 
@@ -146,6 +172,12 @@ void destroyTimer() {
 }
 
 
+
+
+
+
+
+/******************* MAIN *******************/
 
 int main() {
 }
